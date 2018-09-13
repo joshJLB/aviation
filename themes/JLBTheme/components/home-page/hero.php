d89e7b044ce6e195af037ae5dcf05336837caa7a
unknown <josh@jlbworks.com>
2018-09-13 15:06:40 -0500

    Added section one and mobilized

--- themes/JLBTheme/components/home-page/hero.php
+++ themes/JLBTheme/components/home-page/hero.php
@@ -15,7 +15,6 @@
 <?php if (!$hero_video && !$hero_slider): ?>
   <section class="hero" style="background-image: url('<?php echo $hero_background['url']; ?>');" title="<?php echo $hero_background['alt']; ?>">
     <!-- Add Content Here for Static Background -->
-    <div class="overlay"></div>
       <div class="header-content">
           <a class="logo-container" href="<?php echo home_url(); ?>">
             <img class="logo" src="<?=get_field('header_logo', 'option'); ?>"/>
--- themes/JLBTheme/front-page.php
+++ themes/JLBTheme/front-page.php
@@ -12,6 +12,51 @@ get_header(); ?>
 
   <!-- Continue Sections -->
 
+  <section class="one">
+    <div class="one-container">
+      <div class="tab-container">
+        <?php
+          $count = 0; 
+          $count2 = 0;
+        ?>
+        <ul class="nav nav-tabs" role="tablist">
+
+        <?php if(get_field('one_repeater')): ?>
+        <?php while( have_rows('one_repeater') ): the_row(); ?>
+
+          <?php 
+            $count++;
+          ?>
+            <li class="nav-item">
+              <a class="nav-link <?php if($count == 1) { echo 'active'; }; ?>" data-toggle="tab" href="#tab-<?php echo $count; ?>" role="tab">
+                <?=get_sub_field('one_tab_title'); ?>
+              </a>
+            </li>
+          <?php endwhile; ?>
+          <?php endif; ?>
+        </ul>
+
+        <div class="tab-content">
+        
+          <?php if(get_field('one_repeater')): ?>
+          <?php while( have_rows('one_repeater') ): the_row(); ?>
+            
+            <?php
+              $count2++;
+            ?>
+
+            <div class="tab-pane <?php if($count2 == 1) { echo 'active'; }; ?>" id="tab-<?php echo $count2; ?>" role="tabpanel">
+              <div class="content-holder">
+                <h2><?=get_sub_field('one_content_title'); ?></h2>
+                <p><?=get_sub_field('one_content_body'); ?></p>
+                <a href="<?=get_sub_field('one_link_url'); ?>"><?=get_sub_field('one_link_text'); ?></a>
+              </div>
+            </div>
+          <?php endwhile; ?>
+          <?php endif; ?>
+        </div>
+    </div>
+  </section>
   
 </main>
 
